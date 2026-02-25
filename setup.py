b47efec088399dd16642b72c368586adf80ba21b
Korobey <andreykorobey@gmail.com>
2026-02-25 08:10:08 +0300

    feat:  Bitrix24 API client with models

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ setup(
     name="bitrix24-client",
     version="0.1.0",
     packages=find_packages(),
+    url="https://github.com/exprofit/bitrix24-client",
     install_requires=[
-        "httpx>=0.28.0",
-        "pydantic>=2.12.0",
-        "loguru>=0.7.0",
+        "httpx>=0.28.1",
+        "pydantic>=2.12.5",
+        "loguru>=0.7.3",
     ],
-    python_requires=">=3.9",
+    python_requires=">=3.12",
 )
